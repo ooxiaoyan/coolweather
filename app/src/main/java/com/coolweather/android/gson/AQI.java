3c9a48ddc891e0a35db6929b9374df3508bdcfd6
ooxiaoyan <810978527@qq.com>
2018-09-23 20:51:40 +0800

    Add AQI class to describe current air quality

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+public class AQI {
+
+    public AQICity aqiCity;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
