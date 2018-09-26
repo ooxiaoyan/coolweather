3f8902178c6599bbc3d4f90339020becc90b02cc
ooxiaoyan <810978527@qq.com>
2018-09-26 15:44:26 +0800

    Add Timber

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -6,11 +6,21 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**
+         * 仅在Debug时初始化Timber
+         */
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather", null) != null) {
